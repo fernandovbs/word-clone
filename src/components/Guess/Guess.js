742c6193cf3dbf0d52b792071b2b3b74e2bb9814
Fernando Souza <fernandovbsouza@gmail.com>
2023-02-24 21:15:25 -0300

    Remove console.log

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -5,7 +5,6 @@ import { checkGuess } from "../../game-helpers";
 function Guess({guessValue = '', answer }) {
 
   const validatedGuess = checkGuess(guessValue, answer);
-  console.log(validatedGuess); 
   const cells = range(0,5).map(item => (
     <span className={`cell ${validatedGuess ? validatedGuess[item].status : ''}`} key={item}>{guessValue[item] ? guessValue[item] : ''}</span>
   ))
